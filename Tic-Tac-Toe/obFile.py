1703c73351ace06e20a4fa933690ade93aa4ae65
Jonathan Galindez <galindez@bu.edu>
2018-12-23 01:59:09 -0700

    Update 12-22
    Added win condition checking with magic cube calculation.  Added __str__ function. Began, but not finished, __repr__.

--- Tic-Tac-Toe/obFile.py
+++ Tic-Tac-Toe/obFile.py
@@ -5,6 +5,8 @@ Created on Thu Dec 20 16:18:35 2018
 @author: jdgal
 """
 
+
+
 class occupiedSpace(Exception):
     pass
 
@@ -14,17 +16,24 @@ class badInput(Exception):
 class wonGame(Exception):
     pass
 
-class board: 
+class board:
     def __init__(self):
         self.__field__ = []
         self.__owners__ = []
+        self.__playerSums__ = [{"row":[0,0,0], "col":[0,0,0], "diag":[0,0]},
+                                {"row":[0,0,0], "col":[0,0,0], "diag":[0,0]}]
+        self.__boardVals__ = [8,1,6,3,5,7,4,9,2]
         self.turn = 0
         for x in range(3):
             self.__field__.append([False, False, False])
             self.__owners__.append([-1,-1,-1])
     
-    # [] access operator overload. Allows for <Board>[0][0] for easy lookup        
+    def __decodeSpaceVal__(self, row, col):
+        return 1 + 3*(row-1) + (col -1)
+                    
     def __getitem__(self, key):
+        '''[] access operator overload. Allows for <Board>[0][0] for easy
+        lookup'''
         return self.__field__[key]
     
     def __setitem__(self, key, value):
@@ -36,11 +45,38 @@ class board:
         self.__owners__[key] = -1
         
     def __wincheck__(self):
-        #TODO implement graph for finding wins
-        #raise wonGame
-        return
+        for player in self.___playerSums__:
+            for x in range(2):
+                if player["row"][x] >= 15 or player["col"][x] >= 15:
+                    raise wonGame
+            if player["diag"][0] >= 15 or player["diag"][1] >= 15:
+                raise wonGame
+            
+        return False
+    
+    def __updateSums__(self):
+        '''Update winning sum values stored in board, calls wincheck and will
+        raise the exception if there is a winner. Only call this after 
+        directly setting items using <board>[]=x operation as this is not
+        a fast op. Playing the game by using playOn automatically updates
+        the winning sum values meaning this function does not need to be used'''
+        self.__playerSums__ = [{"row":[0,0,0], "col":[0,0,0], "diag":[0,0]},
+                                {"row":[0,0,0], "col":[0,0,0], "diag":[0,0]}]
+        for y in range(3):
+            for x in range(3):
+                if self.__owners__[y][x] != -1: #If space is owned
+                    val = self.__boardVals__[self.__decodeSpaceVal__(x+1, y+1)]
+                    self.__playerSums__[(self.__owners__[y][x])]["row"][x] += val
+                    self.__playerSums__[(self.__owners__[y][x])]["col"][y] += val
+                    if (x-y) == 0: #Downwards Diagonal
+                        self.__playerSums__[(self.__owners__[y][x])]["diag"][0] += val
+                    if (x+y) == 2: #Upwards Diagonal
+                        self.__playerSums__[(self.__owners__[y][x])]["diag"][1] += val
+        
+        self.__wincheck__()
     
     def playOn(self, row, col):
+        val = self.__decodeSpaceVal__(row, col)
         if (row > 3 or row < 1 or col > 3 or row < 1):
             raise badInput
         
@@ -48,7 +84,43 @@ class board:
             raise occupiedSpace
         
         self.__field__[row-1][col-1] = True
-        self.__owners__[row-1][col-1] = self.turn
-        self.__wincheck__()
+        self.__owners__[row-1][col-1] = int(self.turn)
+        self.__playerSums__[self.turn]["row"][row-1] += val
+        self.__playerSums__[self.turn]["col"][col-1] += val
+        if (row == col): #Downwards Diagonal
+            self.__playerSums__[(self.__owners__[col-1][row-1])]["diag"][0] += val
+        if (row + col) == 4: #Upwards Diagonal
+            self.__playerSums__[(self.__owners__[col-1][row-1])]["diag"][1] += val
         self.turn+=1
         self.turn%=2
+    
+    def __str__(self):
+        s = "| - - - - - |\n"
+        for r in range(3):
+            s+= "|"
+            for c in range(3):
+                if self.__field__[r][c]:
+                    if self.__owners__[r][c] == 0:
+                        s+=" X"
+                    else:
+                        s+=" O"
+                    s+= " |"
+                else:
+                    s+="   |"
+                    
+            s+= "\n| - - - - - |\n"
+        return s
+            
+    def __repr__(self):
+        #TODO Complete Full Repr
+        represent = ""
+        represent += "Turn: Player %d\n" % self.turn
+        for p in range(2):
+            represent += "\nPlayer %d Win vals:\n\tRows\n"
+            for x in range(3):
+                represent += str(self.__playerSums__[p]["rows"][x])
+            represent += "\tCols\n"
+            for x in range(3):
+                represent += str(self.__playerSums__[p]["cols"][x])
+                
+        
